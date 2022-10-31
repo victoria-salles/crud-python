7504c68a16f4cc8b21f59dc1d3527eb5a8327470
Victoria <victoria.franca@programmers.com.br>
2022-10-31 17:40:32 -0300

    first commit

--- project/settings.py
+++ project/settings.py
@@ -7,7 +7,6 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 import os
-import django_heroku
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -21,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-)v7o0z)z)l=phpmr@tclxobpu1l=@h%jmhx*ojwmp()emh8ofe'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
@@ -117,7 +116,6 @@ STATIC_URL = '/static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-django_heroku.settings(locals())
 
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 # Default primary key field type
